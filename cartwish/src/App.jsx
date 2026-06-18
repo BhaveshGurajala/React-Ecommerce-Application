f4525ec9b1dc9207be00a358c94fbef026396cd0
BhaveshGurajala <gbhaveshtpt@gmail.com>
2026-06-18 14:36:38 +0530

    Frontend changes

--- cartwish/src/App.jsx
+++ cartwish/src/App.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import HomePage from "./components/Home/HomePage";
+import ProductsPage from "./components/Products/ProductsPage";
+import SingleProductPage from "./components/SingleProduct/SingleProductPage";
+import CartPage from "./components/Cart/CartPage";
+import MyOrderPage from "./components/MyOrder/MyOrderPage";
+import LoginPage from "./components/Authentication/LoginPage";
+import SignupPage from "./components/Authentication/SignupPage";
 
 const App = () => {
   return (
     <div className="app">
       <NavBar />
       <main>
-        <HomePage />
+        {/* <HomePage /> */}
+        {/* <ProductsPage /> */}
+        {/* <SingleProductPage /> */}
+        {/* <CartPage /> */}
+        {/* <MyOrderPage /> */}
+        {/* <LoginPage /> */}
+        <SignupPage />
       </main>
     </div>
   );
---
+++ cartwish/src/components/SingleProduct/SingleProductPage.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./SingleProductPage.css";
+import QuantityInput from "./QuantityInput";
+
+const product = {
+  id: 1,
+  title: "Product Title",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aliquid rerum a? Fugiat soluta facilis deleniti voluptatibus ab architecto dolores a, vero, beatae veniam error doloribus quia laudantium? Error fuga consequuntur quia accusantium? Consequatur modi laboriosam saepe culpa, ab atque.",
+  price: 9.99,
+  images: [
+    "https://static-mpm-optimized.s3.eu-west-2.amazonaws.com/5c1c9eb8-3880-427e-b921-2cf9fe58627e_large.png",
+    "https://static-mpm-optimized.s3.eu-west-2.amazonaws.com/5c1c9eb8-3880-427e-b921-2cf9fe58627e_large.png",
+    "https://static-mpm-optimized.s3.eu-west-2.amazonaws.com/5c1c9eb8-3880-427e-b921-2cf9fe58627e_large.png",
+    "https://static-mpm-optimized.s3.eu-west-2.amazonaws.com/5c1c9eb8-3880-427e-b921-2cf9fe58627e_large.png",
+  ],
+  stock: 10,
+};
+
+const SingleProductPage = () => {
+  const [selectedImage, setSelectedImage] = useState(0);
+  return (
+    <section className="align_center single_product">
+      <div className="align_center">
+        <div className="single_product_thumbnails">
+          {product.images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={product.title}
+              className={selectedImage === index ? "selected_image" : ""}
+              onClick={() => setSelectedImage(index)}
+            />
+          ))}
+        </div>
+        <img
+          src={product.images[selectedImage]}
+          alt={product.title}
+          className="single_product_display"
+        />
+      </div>
+
+      <div className="single_product_details">
+        <h1 className="single_product_title">{product.title}</h1>
+        <p className="single_product_description">{product.description}</p>
+        <p className="single_product_price">${product.price.toFixed(2)}</p>
+        <h2 className="quantity_title">Quantity:</h2>
+
+        <QuantityInput />
+
+        <button className="search_button add_cart">Add to Cart</button>
+      </div>
+    </section>
+  );
+};
+
+export default SingleProductPage;
