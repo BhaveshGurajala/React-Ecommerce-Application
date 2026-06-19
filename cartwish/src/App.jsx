6d76f0fd47c3fcf873f061c4fb07d84b6d2cf09b
BhaveshGurajala <gbhaveshtpt@gmail.com>
2026-06-19 19:21:40 +0530

    Connected to Backend and added pagination and api data handling

--- cartwish/src/App.jsx
+++ cartwish/src/App.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import HomePage from "./components/Home/HomePage";
-import ProductsPage from "./components/Products/ProductsPage";
-import SingleProductPage from "./components/SingleProduct/SingleProductPage";
-import CartPage from "./components/Cart/CartPage";
-import MyOrderPage from "./components/MyOrder/MyOrderPage";
-import LoginPage from "./components/Authentication/LoginPage";
-import SignupPage from "./components/Authentication/SignupPage";
+import Routing from "./components/Routing/Routing";
 
 const App = () => {
   return (
     <div className="app">
       <NavBar />
       <main>
-        {/* <HomePage /> */}
-        {/* <ProductsPage /> */}
-        {/* <SingleProductPage /> */}
-        {/* <CartPage /> */}
-        {/* <MyOrderPage /> */}
-        {/* <LoginPage /> */}
-        <SignupPage />
+        <Routing />
       </main>
     </div>
   );
--- cartwish/src/components/Products/ProductsSidebar.jsx
+++ cartwish/src/components/Products/ProductsSidebar.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import "./ProductsSidebar.css";
 import LinkWithIcon from "../NavBar/LinkWithIcon";
-import rocket from "../../assets/rocket.png";
+import useData from "../../hooks/useData";
 
 const ProductsSidebar = () => {
+  const { data: categories, error } = useData("/category");
+
   return (
     <aside className="products_sidebar">
       <h2>Category</h2>
       <div className="category_links">
-        <LinkWithIcon
-          title="Electronics"
-          link="products?category=electronics"
-          emoji={rocket}
-          sidebar={true}
-        />
+        {error && <em className="form_error">{error}</em>}
+        {categories &&
+          categories.map((category) => (
+            <LinkWithIcon
+              key={category._id}
+              title={category.name}
+              link={`/products?category=${category.name}`}
+              sidebar={true}
+              emoji={`http://localhost:5002/category/${category.image}`}
+            />
+          ))}
       </div>
     </aside>
   );
